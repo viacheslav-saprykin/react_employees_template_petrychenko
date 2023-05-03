6eb5d46324367559ad8773061281f9c59fceeaab
Viacheslav Saprykin <saprykinvp@gmail.com>
2023-05-03 13:32:17 +0300

    133.0

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,18 +1,47 @@
+import { Component } from 'react';
 import './employees-list-item.css';
 
-const EmployeesListItem = ({ name, salary, increase }) => {
-  let classNames = 'list-group-item d-flex justify-content-between';
-  if (increase) {
-    classNames += ' increase';
+class EmployeesListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      increase: false,
+      promotion: false
+    }
+  }
+
+  onIncrease = () => {
+    this.setState(({increase}) => ({
+      increase: !increase
+    }))
+  }
+
+  onPromotion = () => {
+    this.setState(({promotion}) => ({
+      promotion: !promotion
+    }))
   }
   
+  render() {
+    const {name, salary} = this.props;
+    const {increase, promotion} = this.state;
+
+    let classNames = 'list-group-item d-flex justify-content-between';
+    if (increase) {
+      classNames += ' increase';
+    }
+    if (promotion) {
+      classNames += ' like';
+  }
+    
     return (
         <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
+            <span className="list-group-item-label" onClick={this.onPromotion}>{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
-                    className="btn-cookie btn-sm ">
+                    className="btn-cookie btn-sm "
+                    onClick={this.onIncrease}>
                     <i className="fas fa-cookie"></i>
                 </button>
 
@@ -24,6 +53,7 @@ const EmployeesListItem = ({ name, salary, increase }) => {
             </div>
         </li>
     )
-}
+  }
 
+}
 export default EmployeesListItem;
